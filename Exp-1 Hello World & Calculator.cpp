54142416bf7ba60f1ba8880cb447188feb5f759e
Nishka Ranadive <nishka.ranadive@outlook.com>
2025-07-09 19:50:15 +0530

    Add files via upload

---
+++ Exp-1 Hello World & Calculator.cpp
@@ -0,0 +1,43 @@
+//NAME: Nishka Ranadive
+//PRN: 24070123082
+//Exp-1
+
+//Program-1
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    cout<<"Hello World";
+
+    return 0;
+}
+
+
+//Program-2
+ #include<iostream>
+using namespace std;
+
+int main(){
+
+    float num1, num2;
+
+    cout<<"Enter Num1:";
+    cin>>num1;
+    cout<<"Enter Num2:";
+    cin>>num2;
+
+    int sum=num1+num2;
+    cout<<"Addition of Numbers is:"<< sum<< endl;
+
+    int sub=num1-num2;
+    cout<<"Subtraction of Numbers is:"<< sub<< endl;
+
+    int multi=num1*num2;
+    cout<<"Multiplication of Numbers is:"<< multi<< endl;
+
+    float div=num1/num2;
+    cout<<"Division of Numbers is:"<< div;
+    
+    return 0;
+}
